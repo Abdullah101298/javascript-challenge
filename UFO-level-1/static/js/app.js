80e9600d1f401b1e9444b05198539f39d5cc6f12
Abdullah <abdullahalmasri@Abdullahs-MacBook-Air-5.local>
2020-05-08 14:49:06 -0500

    Changed script files

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,33 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Selecting html data from the tbody in index.html 
+var tbody = d3.select("tbody");
+
+
+// Iterating through the data.js script index
+
+tableData.forEach(function(dataiterate) { 
+    console.log(dataiterate);
+
+    // Using d3 to append the tr element in tbodt in the index.html. 
+    var row = tbody.append('tr');
+
+    // 
+
+    Object.entries(dataiterate).forEach(function([key, value]) {console.log(key, value);
+    
+    // 
+    var cell = row.append("td");
+    cell.text(value);
+
+    });
+
+
+    })
+
+
+
+
+
