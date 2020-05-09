ab27ecceee735d500f04690a99cd2b7e4041c166
Abdullah <abdullahalmasri@Abdullahs-MacBook-Air-5.local>
2020-05-09 17:18:43 -0500

    Changes to the script

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -6,10 +6,12 @@ var tableData = data;
 // Selecting html data from the tbody in index.html 
 var tbody = d3.select("tbody");
 
-
 // Iterating through the data.js script index
 
+function tableinsert() { 
+
 tableData.forEach(function(dataiterate) { 
+   
     console.log(dataiterate);
 
     // Using d3 to append the tr element in tbody in the index.html. 
@@ -24,9 +26,44 @@ tableData.forEach(function(dataiterate) {
     });
     })
 
+}
+
+var button = d3.select("#filter-btn");
+
+button.on("click", searchfilter);
+
+function searchfilter() { 
+
+    var inputelement = d3.select("#datetime");
+
+    var inputvalues = inputelement.property("value");
+
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputvalues);
+
+    filteredData.forEach(function(dataiteratefilter) { 
+    
+        // Using d3 to append the tr element in tbody in the index.html. 
+        var rowfilter = tbody.append('tr');
+    
+        Object.entries(dataiteratefilter).forEach(function([keys, values]) {console.log(keys, values);
+        
+        // Appending the td in the html to the values from the data.js
+        var cellfilter = rowfilter.append("td");
+        cellfilter.text(values);
+        });
+        })
+       
+}
+
+tableinsert()
 
 
 
 
+
+
+
+
+    
 
 
