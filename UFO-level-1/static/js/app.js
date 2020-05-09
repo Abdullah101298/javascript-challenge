31cf619d7a2f802f86bc43dd71b5dfcbb67d8862
Abdullah <abdullahalmasri@Abdullahs-MacBook-Air-5.local>
2020-05-09 17:20:57 -0500

    Script changes

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -17,7 +17,6 @@ tableData.forEach(function(dataiterate) {
     // Using d3 to append the tr element in tbody in the index.html. 
     var row = tbody.append('tr');
 
-
     Object.entries(dataiterate).forEach(function([key, value]) {console.log(key, value);
     
     // Appending the td in the html to the values from the data.js
